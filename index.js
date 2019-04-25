c61ebec3004be103e4ceca35938f22e83f8c0d86
Lawrence Yolland <lawrence@sciencedisrupt.com>
2019-04-25 15:58:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+var newArray = [];
+
+function theBeatlesPlay(musicianArray, instrumentArray){
+  for (var i=0; i<musicianArray.length; i++) {
+    newArray[i] = `${musicianArray[i]} plays ${instrumentArray[i]}`
+  }
+  return newArray
+}
